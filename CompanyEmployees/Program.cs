bd1fc0b1d15014a238f7dc2a0c8e5d621806c810
cachorrus <23436971+cachorrus@users.noreply.github.com>
2023-04-20 14:21:30 -0700

    5-Global Error Handling

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,4 +1,5 @@
 using CompanyEmployees.Extensions;
+using Contracts;
 using Microsoft.AspNetCore.HttpOverrides;
 using NLog;
 
@@ -25,13 +26,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}else
-{
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+
+if (app.Environment.IsProduction())
     app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 
--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -19,23 +19,11 @@ namespace Service
 
         public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
         {
-            try
-            {
-                var companies = _repository.Company.GetAllCompanies(trackChanges);
+            var companies = _repository.Company.GetAllCompanies(trackChanges);
 
-               /* var companiesDto = companies.Select(c =>
-                            new CompanyDto(c.Id, c.Name ?? "", string.Join(' ',c.Address, c.Country)))
-                                .ToList();*/
+            var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
 
-                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
-
-                return companiesDto;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Something went wrong in the { nameof(GetAllCompanies) } service method { ex }");
-                throw;
-            }
+            return companiesDto;
         }
     }
 }
