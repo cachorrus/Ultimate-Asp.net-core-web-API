124136ac14c67e3c4bdd9ba2935797afb33cb6a7
cachorrus <23436971+cachorrus@users.noreply.github.com>
2023-04-28 11:02:44 -0700

    11-Working with PUT requests

--- CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -50,5 +50,17 @@ namespace CompanyEmployees.Presentation.Controllers
 
             return NoContent();
         }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpdateEmployeeForCompany(Guid companyId, Guid id, [FromBody] EmployeeForUpdateDto employee)
+        {
+            if (employee is null)
+                return BadRequest("EmployeeForUpdateDto object is null");
+            
+            _service.EmployeeService.UpdateEmployeeForCompany(companyId, id, employee,
+                    compTrackChanges: false, empTrackChanges: true);
+            
+            return NoContent();
+        }
     }
 }
--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -10,5 +10,6 @@ namespace Service.Contracts
         IEnumerable<CompanyDto> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
         (IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection(IEnumerable<CompanyForCreationDto> companyCollection);
         void DeleteCompany(Guid companyId, bool trackChanges);
+        void UpdateCompany(Guid companyid, CompanyForUpdateDto companyForUpdate, bool trackChanges);
     }
 }
